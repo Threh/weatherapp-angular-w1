2be7094807e320ab16f5e25c6742d0a9b865df9d
Threh <93963865+Threh@users.noreply.github.com>
2025-04-09 08:34:55 +0700

    Add files via upload

---
+++ weatherapp - angular - w1/src/app/forecast/pages/currentweather/currentweather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currentweather',
+  imports: [],
+  templateUrl: './currentweather.component.html',
+  styleUrl: './currentweather.component.css'
+})
+export class CurrentweatherComponent {
+
+}
